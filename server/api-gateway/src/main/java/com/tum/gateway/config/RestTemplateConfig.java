779b731c96c5f5e7cddcc260869728375ec8f0df
ge64rov <linn.ewen@outlook.com>
2026-07-09 17:31:32 +0200

    revert changes in api-gateway proposed by coderabbit, because task completion did not work anymore

--- server/api-gateway/src/main/java/com/tum/gateway/config/RestTemplateConfig.java
+++ server/api-gateway/src/main/java/com/tum/gateway/config/RestTemplateConfig.java
@@ -3,17 +3,13 @@ package com.tum.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import java.time.Duration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(5));
-        factory.setReadTimeout(Duration.ofSeconds(30));
-        return new RestTemplate(factory);
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
     }
 }
