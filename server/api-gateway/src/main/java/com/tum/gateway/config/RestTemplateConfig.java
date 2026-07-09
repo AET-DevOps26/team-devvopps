4f543a81e91b7a587d209fac400f0122ca174f1e
ge64rov <linn.ewen@outlook.com>
2026-07-09 17:23:08 +0200

    fix linting problem

--- server/api-gateway/src/main/java/com/tum/gateway/config/RestTemplateConfig.java
+++ server/api-gateway/src/main/java/com/tum/gateway/config/RestTemplateConfig.java
@@ -3,16 +3,17 @@ package com.tum.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5_000);
-        factory.setReadTimeout(30_000);
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        factory.setReadTimeout(Duration.ofSeconds(30));
         return new RestTemplate(factory);
     }
 }
