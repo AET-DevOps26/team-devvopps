9fa58b48d617f7bf4e013cdb24c6c38aab30c33b
ge64rov <linn.ewen@outlook.com>
2026-07-16 02:09:31 +0200

    fix(compose): wait for backend services to be healthy before starting gateway fix(api-gateway): disable unnecessary database auto-configuration

--- server/api-gateway/src/main/java/com/tum/gateway/ApiGatewayApplication.java
+++ server/api-gateway/src/main/java/com/tum/gateway/ApiGatewayApplication.java
@@ -3,7 +3,10 @@ package com.tum.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration.class,
+        org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration.class
+})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
